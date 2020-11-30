2e5ffe99c4e3fd5979f1a798528d3de92ace5918
Suppa <joshsuppa@gmail.com>
2020-11-29 21:34:04 -0500

    Part 3 Completed

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const tagsRouter = express.Router();
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
   console.log("A request is being made to /tags");
@@ -14,6 +14,25 @@ tagsRouter.get("/", (req, res) => {
   res.send({
     tags: [],
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  let tag = req.params.tagName;
+  try {
+    // use our method to get posts by tag name from the db
+    const tagPosts = await getPostsByTagName(tag);
+
+    // filter tagPosts for inactive & current user
+    const filteredPosts = tagPosts.filter((post) => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts: [...filteredPosts] });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
